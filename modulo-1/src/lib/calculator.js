1654b59e72367b43737bb9117f8a97219dcbec5e
Victor Hugo Chimenez <victor.hugo@gringo.com.vc>
2022-02-24 22:45:36 -0300

    chore: finishing first example

--- modulo-1/src/lib/calculator.js
+++ modulo-1/src/lib/calculator.js
@@ -1,3 +1,10 @@
-const sum = (a, b) => a + b
+const sum = (a, b) => {
+    const number1 = parseInt(a)
+    const number2 = parseInt(b)
+
+    if(Number.isNaN(number1) || Number.isNaN(number2)) {
+        throw new Error('Please check your inputs')
+    }
+}
 
 module.exports = sum
